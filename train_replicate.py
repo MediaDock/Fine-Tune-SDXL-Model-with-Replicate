893dfb6ca8e549f7b870d3a023ad44707dff426c
Alexandra Pfammatter <134267719+AlePfa@users.noreply.github.com>
2024-09-06 16:17:36 +0200

    Add files via upload

---
+++ train_replicate.py
@@ -0,0 +1,21 @@
+#Pip install the replicate library before you start
+import replicate
+import os
+
+#You need to create and name a API token on your Replicate account. This will generate a unique ID number/string.
+
+os.environ["REPLICATE_API_TOKEN"]="r8_564sKFhegBuNda8a1e0ehkC8XRfisL24BNLbT" #ID number/string of the generated Replicate token
+
+training = replicate.trainings.create(
+    model="stability-ai/sdxl",
+    version="39ed52f2a78e934b3ba6e2a89f5b1c712de7dfea535525255b1aa35c5565e08b",
+    input={
+      "input_images": "<https://server.com/dataset.zip>", #Link to your dataset (.zip file on a server)
+      "token_string": "TEST", #Name of token
+      "caption_prefix": "a photo of TEST", #Default description of image
+      "max_train_steps": 1000,
+      "use_face_detection_instead": False #Toggle face recocnition on (True) if working with images of faces
+    },
+    #You need to create a model on your Replicate account that will be the destination for the trained version.
+    destination="alepfa/test" #"Path", name of user and model
+)
